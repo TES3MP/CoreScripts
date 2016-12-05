2cc867eaf4a2330aed431e2318665e2c8624bf50
Koncord <koncord@rwa.su>
2016-12-05 21:16:15 +0800

    Move string:split to /lib/lua/utils.lua

--- scripts/myMod.lua
+++ scripts/myMod.lua
@@ -1,10 +1,4 @@
 local time =require('time')
-function string:split(sep)
-    local sep, fields = sep or ":", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) fields[#fields+1] = c end)
-    return fields
-end
 local Methods = {}
 
 Players = {}
