d973d64ce88229aeb25f7b07d0c91a32fd4205b1
Koncord <koncord@rwa.su>
2016-12-05 21:17:18 +0800

    Add useful function for check isModuleExists

--- lib/lua/utils.lua
+++ lib/lua/utils.lua
@@ -5,3 +5,18 @@ function string:split(sep)
     return fields
 end
 
+function isModuleExists(name)
+    if package.loaded[name] then
+        return true
+    else
+        for _, searcher in ipairs(package.searchers or package.loaders) do
+            local loader = searcher(name)
+            if type(loader) == 'function' then
+                package.preload[name] = loader
+                return true
+            end
+        end
+        return false
+    end
+end
+
