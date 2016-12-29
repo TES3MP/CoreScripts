0868b3fdbeecd044be19524cefdce6bc8859b500
Koncord <stas5978@gmail.com>
2016-12-30 02:54:30 +0800

    Update version

--- scripts/server.lua
+++ scripts/server.lua
@@ -35,7 +35,7 @@ function OnServerInit()
 
     local version = tes3mp.GetServerVersion():split(".") -- for future versions
 
-    if tes3mp.GetServerVersion() ~= "0.3.0" then
+    if tes3mp.GetServerVersion() ~= "0.4.1" then
         print("The server or script is outdated!")
         tes3mp.StopServer(1)
     end
