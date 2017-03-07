398ff45fb5c134bc40e17b3211f600b595c6c345
David Cernat <davidcernat@gmail.com>
2017-03-07 05:34:06 +0200

    Replace characters forbidden by operating systems in player filenames

--- scripts/player/file.lua
+++ scripts/player/file.lua
@@ -6,7 +6,12 @@ local Player = class ("Player", BasePlayer)
 function Player:__init(pid)
     BasePlayer.__init(self, pid)
 
-    self.accountFile = self.accountName .. ".txt"
+    -- Replace characters not allowed in filenames
+    self.accountFile = self.accountName
+    self.accountFile = string.gsub(self.accountFile, '[<>:"/\\|*?]', "_")
+    self.accountFile = self.accountFile .. ".txt"
+
+
     if self.hasAccount == nil then
         local home = os.getenv("MOD_DIR").."/player/"
         local file = io.open(home .. self.accountFile, "r")
