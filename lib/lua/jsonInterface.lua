67eee4574799c88b9abf02f19832d38b0a3890a7
David Cernat <davidcernat@gmail.com>
2017-03-28 07:57:01 +0300

    [Libraries] Add basic jsonInterface in case we later switch JSON library

---
+++ lib/lua/jsonInterface.lua
@@ -0,0 +1,21 @@
+local json = require ("dkjson");
+
+local jsonInterface = {};
+
+function jsonInterface.load(fileName)
+    local home = os.getenv("MOD_DIR") .. "/"
+    local file = assert(io.open(home .. fileName, 'r'), 'Error loading file: ' .. fileName);
+    local content = file:read("*all");
+    file:close();
+    return json.decode(content, 1, nil);
+end
+
+function jsonInterface.save(fileName, data)
+    local home = os.getenv("MOD_DIR") .. "/"
+    local content = json.encode(data, { indent = true });
+    local file = assert(io.open(home .. fileName, 'w+b'), 'Error loading file: ' .. fileName);
+    file:write(content);
+    file:close();
+end
+
+return jsonInterface;
