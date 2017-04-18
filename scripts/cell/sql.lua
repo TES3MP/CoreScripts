ea7cf147f9e5ff72d5cb1fb84759441b157ffd3b
David Cernat <davidcernat@gmail.com>
2017-04-18 07:51:46 +0300

    [Cell] Use copy of cell.json as temporary stand-in for cell.sql

--- scripts/cell/sql.lua
+++ scripts/cell/sql.lua
@@ -1,7 +1,46 @@
-local BaseCell = require "cell.base"
+jsonInterface = require("jsonInterface")
+Database = require("database")
+local BaseCell = require("cell.base")
 
-local Cell = class ("Cell", BaseCell)
+local Cell = class("Cell", BaseCell)
 
--- To be filled in later
+function Cell:__init(cellDescription)
+    BaseCell.__init(self, cellDescription)
+
+    -- Replace characters not allowed in filenames
+    self.cellFile = cellDescription
+    self.cellFile = string.gsub(self.cellFile, ":", ";")
+    self.cellFile = self.cellFile .. ".json"
+
+    if self.hasEntry == nil then
+        local home = os.getenv("MOD_DIR").."/cell/"
+        local file = io.open(home .. self.cellFile, "r")
+        if file ~= nil then
+            io.close()
+            self.hasEntry = true
+        else
+            self.hasEntry = false
+        end
+    end
+end
+
+function Cell:CreateEntry()
+    jsonInterface.save("cell/" .. self.cellFile, self.data)
+    self.hasEntry = true
+end
+
+function Cell:Save()
+    if self.hasEntry then
+        jsonInterface.save("cell/" .. self.cellFile, self.data)
+    end
+end
+
+function Cell:Load()
+    self.data = jsonInterface.load("cell/" .. self.cellFile)
+
+    -- JSON doesn't allow numerical keys, but we use them, so convert
+    -- all string number keys into numerical keys
+    tableHelper.fixNumericalKeys(self.data)
+end
 
 return Cell
