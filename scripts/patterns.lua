05d4b3719b24ea02a6e94c30b9bf03ec813abc34
David Cernat <davidcernat@gmail.com>
2017-08-27 05:31:41 +0300

    [Patterns] Allow space after comma in exterior cell pattern

--- scripts/patterns.lua
+++ scripts/patterns.lua
@@ -1,7 +1,7 @@
 patterns = {}
 patterns.invalidFileCharacters = '[<>:"/\\|*?]' -- characters not allowed in filenames
 patterns.commaSplit = "([^, ]+)" -- strings separated by commas
-patterns.exteriorCell = "(%-?%d+),(%-?%d+)$" -- X coordinate, Y coordinate
+patterns.exteriorCell = "(%-?%d+), ?(%-?%d+)$" -- X coordinate, Y coordinate
 patterns.item = "(.+), (%d+), (%-?%d+)$" -- refId, count, charge
 patterns.coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$" -- X coordinate, Y coordinate, Z coordinate
 
