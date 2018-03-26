cbd2c1d739ca6e0472c074c3d092c7b096dd2e71
David Cernat <davidcernat@gmail.com>
2018-03-26 19:40:55 +0300

    [Utils] Add optional enchantmentCharge arg to inventoryHelper methods

--- lib/lua/inventoryHelper.lua
+++ lib/lua/inventoryHelper.lua
@@ -1,25 +1,33 @@
 local inventoryHelper = {};
 
-function inventoryHelper.containsItem(inventory, refId, charge)
+function inventoryHelper.containsItem(inventory, refId, charge, enchantmentCharge)
     for itemIndex, item in pairs(inventory) do
         if item.refId == refId then
-            if charge == nil then
+            if charge == nil and enchantmentCharge == nil then
                 return true
             elseif item.charge == charge then
-                return true
+                if enchantmentCharge == nil then
+                    return true
+                elseif item.enchantmentCharge == enchantmentCharge then
+                    return true
+                end
             end
         end
     end
     return false
 end
 
-function inventoryHelper.getItemIndex(inventory, refId, charge)
+function inventoryHelper.getItemIndex(inventory, refId, charge, enchantmentCharge)
     for itemIndex, item in pairs(inventory) do
         if item.refId == refId then
-            if charge == nil then
+            if charge == nil and enchantmentCharge == nil then
                 return itemIndex
             elseif item.charge == charge then
-                return itemIndex
+                if enchantmentCharge == nil then
+                    return itemIndex
+                elseif item.enchantmentCharge == enchantmentCharge then
+                    return itemIndex
+                end
             end
         end
     end
