5e187b5c5f89fa4eb49ccb4da24e9fbd1d22a97d
David Cernat <davidcernat@gmail.com>
2018-07-28 11:31:43 +0300

    [Utils] Fix commaSplit pattern that was erroneously accepting spaces too

--- lib/lua/patterns.lua
+++ lib/lua/patterns.lua
@@ -1,6 +1,6 @@
 patterns = {}
 patterns.invalidFileCharacters = '[<>:"/\\|*?]' -- characters not allowed in filenames
-patterns.commaSplit = "([^, ]+)" -- strings separated by commas
+patterns.commaSplit = "([^,]+)" -- strings separated by commas
 patterns.exteriorCell = "(%-?%d+), ?(%-?%d+)$" -- X coordinate, Y coordinate
 patterns.item = "(.+), (%d+), (%-?%d+)$" -- refId, count, charge
 patterns.coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$" -- X coordinate, Y coordinate, Z coordinate
