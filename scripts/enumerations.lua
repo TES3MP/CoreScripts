f56220954495c168357265d253dd9e9e1aaeea4d
David Cernat <davidcernat@gmail.com>
2018-08-21 11:16:19 +0300

    [General] Add enumerations for inventory actions

--- scripts/enumerations.lua
+++ scripts/enumerations.lua
@@ -9,6 +9,7 @@ enumerations.aiPrintableAction = { CANCEL = "cancelling current AI", ACTIVATE = 
 enumerations.container = { SET = 0, ADD = 1, REMOVE = 2 }
 enumerations.containerSub = { NONE = 0, DRAG = 1, DROP = 2, TAKE_ALL = 3, REPLY_TO_REQUEST = 4 }
 enumerations.faction = { RANK = 0, EXPULSION = 1, REPUTATION = 2 }
+enumerations.inventory = { SET = 0, ADD = 1, REMOVE = 2 }
 enumerations.journal = { ENTRY = 0, INDEX = 1 }
 enumerations.miscellaneous = { MARK_LOCATION = 0, SELECTED_SPELL = 1 }
 enumerations.objectCategories = { PLAYER = 0, ACTOR = 1, PLACED_OBJECT = 2 }
