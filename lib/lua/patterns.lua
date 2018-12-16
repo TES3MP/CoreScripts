602e23bb29cbf1118f9eeb6d414ae50f99d3cedb
David Cernat <davidcernat@gmail.com>
2018-12-16 13:19:45 +0200

    [Utils] Add periodSplit pattern

--- lib/lua/patterns.lua
+++ lib/lua/patterns.lua
@@ -1,6 +1,7 @@
 patterns = {}
 patterns.invalidFileCharacters = '[<>:"/\\|*?]' -- characters not allowed in filenames
 patterns.commaSplit = "%s*([^,]+)" -- strings separated by commas, with spaces immediately after the commas ignored
+patterns.periodSplit = "%s*([^%.]+)" -- as in commaSplit, but with periods
 patterns.exteriorCell = "(%-?%d+), ?(%-?%d+)$" -- X coordinate, Y coordinate
 patterns.item = "(.+), (%d+), (%-?%d+)$" -- refId, count, charge
 patterns.coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$" -- X coordinate, Y coordinate, Z coordinate
