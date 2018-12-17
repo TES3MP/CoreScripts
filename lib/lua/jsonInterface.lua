244b64ffcf904b820f773cf34b7c2a6456783a3d
David Cernat <davidcernat@gmail.com>
2018-12-17 12:21:27 +0200

    [JsonInterface] Stop using assert in load() and check if file is nil
    Additionally, remove semicolons from the ends of lines, for consistency with other scripts.

--- lib/lua/jsonInterface.lua
+++ lib/lua/jsonInterface.lua
@@ -1,27 +1,32 @@
-local json = require ("dkjson");
+local json = require ("dkjson")
 
-local jsonInterface = {};
+local jsonInterface = {}
 
 function jsonInterface.load(fileName)
     local home = os.getenv("MOD_DIR") .. "/"
-    local file = assert(io.open(home .. fileName, 'r'), 'Error loading file: ' .. fileName);
-    local content = file:read("*all");
-    file:close();
-    return json.decode(content, 1, nil);
+    local file = io.open(home .. fileName, 'r')
+
+    if file ~= nil then
+        local content = file:read("*all")
+        file:close()
+        return json.decode(content, 1, nil)
+    else
+        return nil
+    end
 end
 
 function jsonInterface.save(fileName, data, keyOrderArray)
     local home = os.getenv("MOD_DIR") .. "/"
-    local content = json.encode(data, { indent = true, keyorder = keyOrderArray });
-    local file = io.open(home .. fileName, 'w+b');
+    local content = json.encode(data, { indent = true, keyorder = keyOrderArray })
+    local file = io.open(home .. fileName, 'w+b')
 
     if file ~= nil then
-        file:write(content);
-        file:close();
+        file:write(content)
+        file:close()
         return true
     else
         return false
     end
 end
 
-return jsonInterface;
+return jsonInterface
