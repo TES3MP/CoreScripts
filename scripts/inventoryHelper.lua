be24a0f8f5018d2ed0f83e8aebc769cda3e7aa0e
David Cernat <davidcernat@gmail.com>
2019-01-16 08:50:40 +0200

    [InventoryHelper] Add removeClosestItem() and compareClosenessToItem()
    Add getItemIndexes()
    
    Turn the previous removeItem() into removeExactItem() and remove its return value because it had no use.
    
    Make the new removeItem() call removeClosestItem() instead.

--- scripts/inventoryHelper.lua
+++ scripts/inventoryHelper.lua
@@ -23,6 +23,7 @@ function inventoryHelper.containsItem(inventory, refId, charge, enchantmentCharg
 end
 
 function inventoryHelper.getItemIndex(inventory, refId, charge, enchantmentCharge, soul)
+
     for itemIndex, item in pairs(inventory) do
         if item.refId == refId then
 
@@ -44,6 +45,19 @@ function inventoryHelper.getItemIndex(inventory, refId, charge, enchantmentCharg
     return nil
 end
 
+function inventoryHelper.getItemIndexes(inventory, refId)
+
+    local indexes = {}
+
+    for itemIndex, item in pairs(inventory) do
+        if item.refId == refId then
+            table.insert(indexes, itemIndex)
+        end
+    end
+
+    return indexes
+end
+
 function inventoryHelper.addItem(inventory, refId, count, charge, enchantmentCharge, soul)
 
     if inventoryHelper.containsItem(inventory, refId, charge, enchantmentCharge, soul) then
@@ -62,7 +76,137 @@ function inventoryHelper.addItem(inventory, refId, count, charge, enchantmentCha
     end
 end
 
-function inventoryHelper.removeItem(inventory, refId, count, charge, enchantmentCharge, soul)
+-- Return true if an item (comparedItem) is closer to a desired item (idealItem) than
+-- another item is (otherItem)
+function inventoryHelper.compareClosenessToItem(idealItem, comparedItem, otherItem)
+
+    if comparedItem == otherItem then
+        return false
+    end
+
+    -- A difference in refIds instantly resolves the comparison
+    if idealItem.refId ~= nil and not comparedItem.refId:ciEqual(otherItem.refId) then
+        if idealItem.refId:ciEqual(comparedItem.refId) then
+            return true
+        elseif idealItem.refId:ciEqual(otherItem.refId) then
+            return false
+        end
+    end
+
+    -- A difference in souls also instantly resolves the comparison
+    if idealItem.soul ~= nil and not comparedItem.soul:ciEqual(otherItem.soul) then
+        if idealItem.soul:ciEqual(comparedItem.soul) then
+            return true
+        elseif idealItem.soul:ciEqual(otherItem.soul) then
+            return false
+        end
+    end
+
+    -- The TES3MP server doesn't yet load up data files, so it doesn't actually know what the
+    -- maximum charge and enchantmentCharge are supposed to be for a particular refId
+    --
+    -- Use some dirty workarounds here to ignore that fact until the sensible and elegant
+    -- solution becomes available
+
+    local comparedChargeDiff, otherChargeDiff = 0, 0
+    local comparedEnchantmentChargeDiff, otherEnchantmentChargeDiff = 0, 0
+
+    if idealItem.charge ~= nil and comparedItem.charge ~= otherItem.charge then
+
+        local maxValue = math.max(idealItem.charge, comparedItem.charge, otherItem.charge)
+
+        if maxValue < 400 then maxValue = maxValue + 400 end
+
+        local adjustedIdealCharge = idealItem.charge
+        local adjustedComparedCharge = comparedItem.charge
+        local adjustedOtherCharge = otherItem.charge
+
+        if adjustedIdealCharge == -1 then adjustedIdealCharge = maxValue + maxValue / 2 end
+        if adjustedComparedCharge == -1 then adjustedComparedCharge = maxValue + maxValue / 2 end
+        if adjustedOtherCharge == -1 then adjustedOtherCharge = maxValue + maxValue / 2 end
+
+        comparedChargeDiff = math.abs(adjustedIdealCharge - adjustedComparedCharge)
+        otherChargeDiff = math.abs(adjustedIdealCharge - adjustedOtherCharge)
+    end
+
+    if idealItem.enchantmentCharge ~= nil and comparedItem.enchantmentCharge ~= otherItem.enchantmentCharge then
+
+        local maxValue = math.max(idealItem.enchantmentCharge, comparedItem.enchantmentCharge, otherItem.enchantmentCharge)
+
+        if maxValue < 200 then maxValue = maxValue + 200 end
+
+        local adjustedIdealEnchantmentCharge = idealItem.enchantmentCharge
+        local adjustedComparedEnchantmentCharge = comparedItem.enchantmentCharge
+        local adjustedOtherEnchantmentCharge = otherItem.enchantmentCharge
+
+        if adjustedIdealEnchantmentCharge == -1 then adjustedIdealEnchantmentCharge = maxValue + maxValue / 2 end
+        if adjustedComparedEnchantmentCharge == -1 then adjustedComparedEnchantmentCharge = maxValue + maxValue / 2 end
+        if adjustedOtherEnchantmentCharge == -1 then adjustedOtherEnchantmentCharge = maxValue + maxValue / 2 end
+
+        comparedEnchantmentChargeDiff = math.abs(adjustedIdealEnchantmentCharge - adjustedComparedEnchantmentCharge)
+        otherEnchantmentChargeDiff = math.abs(adjustedIdealEnchantmentCharge - adjustedOtherEnchantmentCharge)
+    end
+
+    if comparedChargeDiff + comparedEnchantmentChargeDiff < otherChargeDiff + otherEnchantmentChargeDiff then
+        return true
+    end
+
+    return false
+end
+
+function inventoryHelper.removeClosestItem(inventory, refId, count, charge, enchantmentCharge, soul)
+
+    if inventoryHelper.containsItem(inventory, refId) then
+        local itemIndexesToCompare = inventoryHelper.getItemIndexes(inventory, refId)
+        local itemIndexesByCloseness = {}
+        local idealItem = { refId = refId, charge = charge, enchantmentCharge = enchantmentCharge,
+            soul = soul }
+
+        for _, comparedItemIndex in ipairs(itemIndexesToCompare) do
+
+            local comparedItem = inventory[comparedItemIndex]
+            local isLeastClose = true
+
+            for closenessRanking, otherItemIndex in ipairs(itemIndexesByCloseness) do
+                local otherItem = inventory[otherItemIndex]
+
+                if inventoryHelper.compareClosenessToItem(idealItem, comparedItem, otherItem) then
+                    table.insert(itemIndexesByCloseness, closenessRanking, comparedItemIndex)
+                    isLeastClose = false
+                    break
+                end
+            end
+
+            if isLeastClose then
+                table.insert(itemIndexesByCloseness, comparedItemIndex)
+            end
+        end
+
+        local remainingCount = count
+
+        for closenessRanking, currentItemIndex in ipairs(itemIndexesByCloseness) do
+
+            if remainingCount > 0 then
+                local currentItem = inventory[currentItemIndex]
+
+                currentItem.count = currentItem.count - remainingCount
+
+                if currentItem.count < 1 then
+                    remainingCount = 0 - currentItem.count
+                    currentItem = nil
+                else
+                    remainingCount = 0
+                end
+
+                inventory[currentItemIndex] = currentItem
+            else
+                break
+            end
+        end
+    end
+end
+
+function inventoryHelper.removeExactItem(inventory, refId, count, charge, enchantmentCharge, soul)
 
     if inventoryHelper.containsItem(inventory, refId, charge, enchantmentCharge, soul) then
         local index = inventoryHelper.getItemIndex(inventory, refId, charge, enchantmentCharge, soul)
@@ -72,11 +216,12 @@ function inventoryHelper.removeItem(inventory, refId, count, charge, enchantment
         if inventory[index].count < 1 then
             inventory[index] = nil
         end
-
-        return inventory[index]
     end
+end
 
-    return nil
+-- Deprecated
+function inventoryHelper.removeItem(inventory, refId, count, charge, enchantmentCharge, soul)
+    return inventoryHelper.removeClosestItem(inventory, refId, count, charge, enchantmentCharge, soul)
 end
 
 return inventoryHelper
