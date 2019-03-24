1029c0157bc243bccf70bab7bac92079738f4ebe
David Cernat <davidcernat@gmail.com>
2019-03-24 18:36:05 +0200

    [Documentation] Add basic instructions to customScripts.lua

---
+++ scripts/customScripts.lua
@@ -0,0 +1,7 @@
+-- Load up your custom scripts here! Ideally, your custom scripts will be placed in the scripts/custom folder and then get loaded like this:
+--
+-- require("custom/yourScript")
+--
+-- Refer to the Tutorial.md file for information on how to use various event and command hooks in your scripts.
+
+
