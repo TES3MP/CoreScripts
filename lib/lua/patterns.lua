4370fd2288701cd3e3e62f3ce897223a336ba9a9
1338 <maxvandersluis@yahoo.com>
2019-05-24 15:45:25 +0200

    Modifying patterns to LDOC compatible structure and adding LDOC string

--- lib/lua/patterns.lua
+++ lib/lua/patterns.lua
@@ -1,9 +1,15 @@
-patterns = {}
-patterns.invalidFileCharacters = '[<>:"/\\|*?\r\n]' -- characters not allowed in filenames
-patterns.commaSplit = "%s*([^,]+)" -- strings separated by commas, with spaces immediately after the commas ignored
-patterns.periodSplit = "%s*([^%.]+)" -- as in commaSplit, but with periods
-patterns.exteriorCell = "(%-?%d+), ?(%-?%d+)$" -- X coordinate, Y coordinate
-patterns.item = "(.+), (%d+), (%-?%d+)$" -- refId, count, charge
-patterns.coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$" -- X coordinate, Y coordinate, Z coordinate
+--- Usefull patterns that can be used
+-- @module patterns
+
+--- Usefull patterns
+-- @table patterns
+patterns = {
+    invalidFileCharacters = '[<>:"/\\|*?\r\n]', -- characters not allowed in filenames
+    commaSplit = "%s*([^,]+)", -- strings separated by commas, with spaces immediately after the commas ignored
+    periodSplit = "%s*([^%.]+)", -- as in commaSplit, but with periods
+    exteriorCell = "(%-?%d+), ?(%-?%d+)$", -- X coordinate, Y coordinate
+    item = "(.+), (%d+), (%-?%d+)$", -- refId, count, charge
+    coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$"  -- X coordinate, Y coordinate, Z coordinate
+}
 
 return patterns
