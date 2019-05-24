d525908207444c83b9feb15df2100003d6710674
1338 <maxvandersluis@yahoo.com>
2019-05-24 15:36:14 +0200

    Adding LDOC to file

--- lib/lua/fileHelper.lua
+++ lib/lua/fileHelper.lua
@@ -1,14 +1,19 @@
+--- An file helper
+-- @module fileHelper
+
 require("patterns")
 tableHelper = require("tableHelper")
 
 local fileHelper = {}
 
--- Avoid using the following filenames because of their reserved status on operating systems
+--- Avoid using the following filenames because of their reserved status on operating systems
 fileHelper.invalidFilenames = { "CON", "PRN", "AUX", "NUL", "COM1", "COM2", "COM3", "COM4", "COM5",
     "COM6", "COM7", "COM8", "COM9", "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7",
     "LPT8", "LPT9" }
 
 -- Turn an invalid filename into a valid one
+-- @param filename file name
+-- @return fixed filename
 function fileHelper.fixFilename(filename)
     
     -- Replace characters not allowed in filenames
