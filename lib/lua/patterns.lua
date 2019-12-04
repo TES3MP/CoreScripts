ba743c915c01f6aa098b9e34440632cc79ff5eca
David Cernat <davidcernat@gmail.com>
2019-12-04 15:48:36 +0200

    [Patterns] Add quoteSplit

--- lib/lua/patterns.lua
+++ lib/lua/patterns.lua
@@ -2,6 +2,7 @@ patterns = {}
 patterns.invalidFileCharacters = '[<>:"/\\|*?\r\n]' -- characters not allowed in filenames
 patterns.commaSplit = "%s*([^,]+)" -- strings separated by commas, with spaces immediately after the commas ignored
 patterns.periodSplit = "%s*([^%.]+)" -- as in commaSplit, but with periods
+patterns.quoteSplit = '".-"' -- strings separated by quotation marks
 patterns.exteriorCell = "(%-?%d+), ?(%-?%d+)$" -- X coordinate, Y coordinate
 patterns.item = "(.+), (%d+), (%-?%d+)$" -- refId, count, charge
 patterns.coordinates = "(%-?%d+%.?%d*), (%-?%d+%.?%d*), (%-?%d+%.?%d*)$" -- X coordinate, Y coordinate, Z coordinate
