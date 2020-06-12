87b6ef243ada3afd7afc4e5ec49deca2e6d2b94e
Anton Uramer <antonuramer@gmail.com>
2020-06-13 00:05:18 +0200

    Add async gui functions to guiHelper

--- scripts/guiHelper.lua
+++ scripts/guiHelper.lua
@@ -3,6 +3,84 @@ tableHelper = require("tableHelper")
 guiHelper = {}
 guiHelper.names = {"LOGIN", "REGISTER", "PLAYERSLIST", "CELLSLIST"}
 guiHelper.ID = tableHelper.enum(guiHelper.names)
+guiHelper.currentId = 0
+for _, id in pairs(guiHelper.ID) do
+    guiHelper.currentId = math.max(guiHelper.currentId, id)
+end
+guiHelper.coroutines = {}   
+
+function guiHelper.GetGuiId(name)
+    if not name then
+        guiHelper.currentId = guiHelper.currentId + 1
+        return guiHelper.currentId
+    elseif guiHelper.ID[name] then
+        return guiHelper.ID[name]
+    else
+        guiHelper.currentId = guiHelper.currentId + 1
+        guiHelper.ID[name] = guiHelper.currentId
+        return guiHelper.currentId
+    end
+end
+
+function guiHelper.SetCoroutine(id)
+    guiHelper.coroutines[id] = async.CurrentCoroutine()
+end
+
+function guiHelper.MessageBox(pid, label)
+    local id = guiHelper.GetGuiId("GUI_HELPER_MESSAGEBOX")
+    tes3mp.MessageBox(pid, id, label)
+    return true
+end
+
+function guiHelper.CustomMessageBoxAsync(pid, buttons, label)
+    local id = guiHelper.GetGuiId()
+    guiHelper.SetCoroutine(id)
+    label = label or ''
+    tes3mp.CustomMessageBox(pid, id, label, table.concat(buttons, ";"))
+    return coroutine.yield() + 1 -- Lua tables are 1-numbered
+end
+
+function guiHelper.InputDialogAsync(pid, label, note)
+    local id = guiHelper.GetGuiId()
+    guiHelper.SetCoroutine(id)
+    note = note or ''
+    label = label or ''
+    tes3mp.InputDialog(pid, id, label, note)
+    return coroutine.yield()
+end
+
+function guiHelper.PasswordDialogAsync(pid, label, note)
+    local id = guiHelper.GetGuiId()
+    guiHelper.SetCoroutine(id)
+    label = label or ''
+    note = note or ''
+    tes3mp.PasswordDialog(pid, id, label, note)
+    return coroutine.yield()
+end
+
+function guiHelper.ListBoxAsync(pid, rows, label)
+    local id = guiHelper.GetGuiId()
+    guiHelper.SetCoroutine(id)
+    local items = table.concat(rows, "\n")
+    label = label or ''
+    tes3mp.ListBox(pid, id, label, items)
+    local result = tonumber(coroutine.yield())
+    if (result >= #rows) or (result < 0) then -- user didn't select any options
+        return nil
+    end
+    return result + 1 -- Lua tables are 1-numbered
+end
+
+function guiHelper.OnGUIAction(evenStatus, pid, id, data)
+    if evenStatus.validDefaultHandler then
+        local co = guiHelper.coroutines[id]
+        if co then
+            guiHelper.coroutines[id] = nil
+            coroutine.resume(co, data)
+        end
+    end
+end
+customEventHooks.registerHandler("OnGUIAction", guiHelper.OnGUIAction)
 
 guiHelper.ShowLogin = function(pid)
     tes3mp.PasswordDialog(pid, guiHelper.ID.LOGIN, "Enter your password:", "")
