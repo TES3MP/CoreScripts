9f63f09628be93aea7ffceae438e00b16afa5cb2
David Cernat <davidcernat@gmail.com>
2021-07-22 11:16:22 +0200

    [ContentFixer] Synchronize corprus affliction & cure with shared journal

--- scripts/contentFixer.lua
+++ scripts/contentFixer.lua
@@ -65,4 +65,86 @@ function contentFixer.UnequipDeadlyItems(pid)
     end
 end
 
+function contentFixer.AdjustSharedCorprusState(pid)
+
+    local corprusId = "corprus"
+
+    if WorldInstance.data.customVariables.corprusCured == true then
+        if tableHelper.containsValue(Players[pid].data.spellbook, corprusId) == true then
+        
+            tableHelper.removeValue(Players[pid].data.spellbook, corprusId)
+            tableHelper.cleanNils(Players[pid].data.spellbook)
+
+            tes3mp.ClearSpellbookChanges(pid)
+            tes3mp.SetSpellbookChangesAction(pid, enumerations.spellbook.REMOVE)
+            tes3mp.AddSpell(pid, corprusId)
+            tes3mp.SendSpellbookChanges(pid)
+
+            tes3mp.ClearSpellbookChanges(pid)
+            tes3mp.SetSpellbookChangesAction(pid, enumerations.spellbook.ADD)
+            for _, spellId in ipairs({"common disease immunity", "blight disease immunity","corprus immunity"}) do
+                table.insert(Players[pid].data.spellbook, spellId)
+                tes3mp.AddSpell(pid, spellId)
+            end
+            tes3mp.SendSpellbookChanges(pid)
+            tes3mp.MessageBox(pid, -1, "You have been cured of corprus.")
+        end
+    elseif WorldInstance.data.customVariables.corprusGained == true then
+        if tableHelper.containsValue(Players[pid].data.spellbook, corprusId) == false then
+
+            table.insert(Players[pid].data.spellbook, corprusId)
+
+            tes3mp.ClearSpellbookChanges(pid)
+            tes3mp.SetSpellbookChangesAction(pid, enumerations.spellbook.ADD)
+            tes3mp.AddSpell(pid, corprusId)
+            tes3mp.SendSpellbookChanges(pid)
+            tes3mp.MessageBox(pid, -1, "You have been afflicted with corprus.")
+        end
+    end
+end
+
+function contentFixer.AdjustWorldCorprusVariables(journal)
+
+    local madeAdjustment = false
+
+    for _, journalItem in ipairs(journal) do
+
+        if journalItem.quest == "a2_3_corpruscure" and journalItem.index >= 50 then
+            WorldInstance.data.customVariables.corprusCured = true
+            madeAdjustment = true
+        elseif journalItem.quest == "a2_2_6thhouse" and journalItem.index >= 50 then
+            WorldInstance.data.customVariables.corprusGained = true
+            madeAdjustment = true
+        end
+    end
+
+    return madeAdjustment
+end
+
+customEventHooks.registerHandler("OnPlayerJournal", function(eventStatus, pid, playerPacket)
+    if config.shareJournal == true then
+        local madeAdjustment = contentFixer.AdjustWorldCorprusVariables(playerPacket.journal)
+
+        if madeAdjustment == true then
+            for otherPid, otherPlayer in pairs(Players) do
+                if otherPid ~= pid then
+                    contentFixer.AdjustSharedCorprusState(otherPid)
+                end
+            end
+        end
+    end
+end)
+
+customEventHooks.registerHandler("OnPlayerFinishLogin", function(eventStatus, pid)
+    if config.shareJournal == true then
+        contentFixer.AdjustSharedCorprusState(pid)
+    end
+end)
+
+customEventHooks.registerHandler("OnWorldReload", function(eventStatus)
+    if config.shareJournal == true then
+        contentFixer.AdjustWorldCorprusVariables(WorldInstance.data.journal)
+    end
+end)
+
 return contentFixer
