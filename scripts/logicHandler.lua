58fa94e83886ff6591039abe7506491b29c89200
Rickoff <38435435+rickoff@users.noreply.github.com>
2022-02-16 13:33:02 +0100

    add scale in createObject (#106)
    * add scale in createObject
    
    * Update logicHandler.lua
    
    * Update dataTableBuilder.lua

--- scripts/logicHandler.lua
+++ scripts/logicHandler.lua
@@ -452,6 +452,7 @@ logicHandler.CreateObjects = function(cellDescription, objectsToCreate, packetTy
         local enchantmentCharge = object.enchantmentCharge
         local soul = object.soul
         local location = object.location
+        local scale = object.scale
 
         local mpNum = WorldInstance:GetCurrentMpNum() + 1
         local uniqueIndex =  0 .. "-" .. mpNum
@@ -491,13 +492,17 @@ logicHandler.CreateObjects = function(cellDescription, objectsToCreate, packetTy
             cell:InitializeObjectData(uniqueIndex, refId)
             cell.data.objectData[uniqueIndex].location = location
 
+            if scale ~= nil then
+                cell.data.objectData[uniqueIndex].scale = scale
+                table.insert(cell.data.packets.scale, uniqueIndex)
+            end
+
             if packetType == "place" then
                 table.insert(cell.data.packets.place, uniqueIndex)
             elseif packetType == "spawn" then
                 table.insert(cell.data.packets.spawn, uniqueIndex)
                 table.insert(cell.data.packets.actorList, uniqueIndex)
             end
-
             -- Are there any players on the server? If so, initialize the object
             -- list for the first one we find and just send the corresponding packet
             -- to everyone
@@ -509,6 +514,7 @@ logicHandler.CreateObjects = function(cellDescription, objectsToCreate, packetTy
                 tes3mp.SetObjectRefId(refId)
                 tes3mp.SetObjectRefNum(0)
                 tes3mp.SetObjectMpNum(mpNum)
+                tes3mp.SetObjectScale(scale)
 
                 if packetType == "place" then
                     tes3mp.SetObjectCount(count)
@@ -549,6 +555,10 @@ logicHandler.CreateObjects = function(cellDescription, objectsToCreate, packetTy
         elseif packetType == "spawn" then
             tes3mp.SendObjectSpawn(true)
         end
+
+        if scale ~= nil then
+            tes3mp.SendObjectScale(true)
+        end
     end
 
     cell:Save()
@@ -570,6 +580,7 @@ logicHandler.CreateObjectAtLocation = function(cellDescription, location, object
         charge = objectData.charge,
         enchantmentCharge = objectData.enchantmentCharge,
         soul = objectData.soul,
+        scale = objectData.scale,
         packetType = packetType
         }
     )
