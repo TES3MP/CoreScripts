0b994dcac6bc3bbda669820f031f8b5b8264b7b4
Sidney Keese <me@sidke.com>
2022-06-13 22:16:37 -0700

    the timer docs existed outside of the Functions directory

--- types/_class_timer_functions.lua
+++ types/_class_timer_functions.lua
@@ -1,31 +1,42 @@
 ---@class TES3MP
 local api
 
----@param callback string
----@param msec integer
----@return integer
+---Create a timer that will run a script function after a certain interval.
+---@param callback string @The Lua script function.
+---@param msec integer @The interval in miliseconds.
+---@return integer @The ID of the timer thus created.
 function api.CreateTimer(callback, msec) end
 
----@param callback string
----@param msec integer
----@param types string
----@param ... any
----@return integer
+---Create a timer that will run a script function after a certain interval and pass certain arguments to it.
+---
+---Example usage:
+--- - tes3mp.CreateTimerEx("OnTimerTest1", 250, "i", 90)
+--- - tes3mp.CreateTimerEx("OnTimerTest2", 500, "sif", "Test string", 60, 77.321)
+---@param callback unknown @The Lua script function.
+---@param msec integer @The interval in miliseconds.
+---@param types string @The argument types.
+---@param ... any @The arguments.
+---@return integer @The ID of the timer thus created.
 function api.CreateTimerEx(callback, msec, types, ...) end
 
----@param timerId integer
+---Start the timer with a certain ID.
+---@param timerId integer @The timer ID.
 function api.StartTimer(timerId) end
 
----@param timerId integer
+---Stop the timer with a certain ID.
+---@param timerId integer @The timer ID.
 function api.StopTimer(timerId) end
 
----@param timerId integer
----@param msec integer
+---Restart the timer with a certain ID for a certain interval.
+---@param timerId integer @The timer ID.
+---@param msec integer @The interval in miliseconds.
 function api.RestartTimer(timerId, msec) end
 
----@param timerId integer
+---Free the timer with a certain ID.
+---@param timerId integer @The timer ID.
 function api.FreeTimer(timerId) end
 
----@param timerId integer
----@return boolean
+---Check whether a timer is elapsed.
+---@param timerId integer @The timer ID.
+---@return boolean @Whether the timer is elapsed.
 function api.IsTimerElapsed(timerId) end
